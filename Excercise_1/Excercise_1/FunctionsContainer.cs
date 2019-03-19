36676950b1e37cb6d049799f8ec1fb74a11fa667
Afik Aharon <afik.aharon@live.biu.ac.il>
2019-03-19 23:36:19 +0200

    First commit

---
+++ Excercise_1/Excercise_1/FunctionsContainer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Excercise_1{
+    class FunctionsContainer {
+        public delegate void intDelegate(int val);
+        Dictionary<string, intDelegate> container = new Dictionary<String, intDelegate>();
+        public intDelegate this[string idx]
+        {
+            get
+            {
+                return container[idx];
+            }
+            set
+            {
+                container[idx] = value;
+            }
+        }
+    }
+}
